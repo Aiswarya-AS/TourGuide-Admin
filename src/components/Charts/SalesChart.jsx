f1752d3d76d9eddbd27f405e5da2efe2dbf42dee
Aiswarya-AS <aiswaryaasubash@gmail.com>
2023-05-25 14:49:03 +0530

    first commit

---
+++ src/components/Charts/SalesChart.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Chart from 'react-apexcharts';
+
+const SalesChart = ({completed,started,cancelled,dates}) => {
+
+  const chartOptions = {
+    chart: {
+      id: 'line-chart',
+    },
+    xaxis: {
+      categories: dates.map((d)=>d.is_created__date)
+    },
+  };
+
+  const chartSeries = [
+    {
+      name: 'Trips Completed',
+      data: completed.map((c)=>c.completed)
+    },
+    {
+      name: 'Started Trips',
+      data: started.map((s)=>s.started)
+    },
+    {
+      name: 'Cancelled Trips',
+      data: cancelled.map((c)=>c.cancelled)
+    },
+  ];
+  return (
+    <div>
+      <Chart
+      options={chartOptions}
+      series={chartSeries}
+      type="line"
+      height={400}
+    />
+    </div>
+  )
+}
+
+export default SalesChart
